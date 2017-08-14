f16cceaa6011017fa8f80358001ecf8142c71af8
Tien <truong.htien@gmail.com>
2017-08-14 14:40:16 -0400

    users.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     NgbModule.forRoot(),
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'users',
+      component: UsersComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/users.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'UsersComponent',
+  templateUrl: './users.component.html',
+})
+export class UsersComponent {
+}
