714647a2393912d47be26da47f83a8160884fb68
Tien <truong.htien@gmail.com>
2017-08-14 15:49:15 -0400

    users - table

--- src/app/users.component.ts
+++ src/app/users.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
+const USERS = [{
+  id: 1,
+  name: "toto"
+}];
+
 @Component({
   selector: 'UsersComponent',
   templateUrl: './users.component.html',
 })
 export class UsersComponent {
+  public users = USERS;
 }
